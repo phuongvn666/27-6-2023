c5dd1e878493d3cba9afa6e49ef5b0c8a3a61270
phuongvn666 <93229788+phuongvn666@users.noreply.github.com>
2023-06-27 13:53:58 +0700

    Class6.cs

--- Class6.cs
+++ Class6.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +12,7 @@ namespace _27_6
         {
             Example6();
         }
-        static int Example6()
+        static void Example6()
         {
             Console.WriteLine("- - - - - - - - - - - - -  -");
             string Number;
@@ -24,6 +24,12 @@ namespace _27_6
                 soThu1 = Convert.ToInt32(Number);
             }
             while (soThu1 <= 0);
+            int Tich = GiaiThuat6(soThu1);
+            Console.WriteLine($"Giai thua cua {soThu1} : {Tich}");
+            Console.WriteLine("- - - - - - - - - - - - -  -");
+        }
+        static int GiaiThuat6(int soThu1)
+        {
             int soAo = soThu1;
             int Tich = 1;
             while (soAo > 0)
@@ -31,9 +37,7 @@ namespace _27_6
                 Tich *= soAo;
                 soAo -= 1;
             };
-            Console.WriteLine($"Giai thua cua {soThu1} : {Tich}");
-            Console.WriteLine("- - - - - - - - - - - - -  -");
-            return soThu1;
+            return Tich;
         }
     }
 }
